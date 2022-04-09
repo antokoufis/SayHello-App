ab8c75c023788efadb238ddfb63d92b447e1bfae
antokoufis <antokoufis@gmail.com>
2022-04-09 21:50:48 +0300

    Creates the new Activity

---
+++ app/src/main/java/com/example/sayhelloapp/SayHelloNewScreen.java
@@ -0,0 +1,14 @@
+package com.example.sayhelloapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SayHelloNewScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_say_hello_new_screen);
+    }
+}
